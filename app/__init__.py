027b42fce3ff4532cd94a7cd760be04b26834ec9
Alex Gerstein <gerstein.alex@gmail.com>
2015-01-14 15:50:32 -0500

    Add logging for Heroku.

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 from app import views
+
+# Logging
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('rss-aggregator startup')
