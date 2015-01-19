a2790a85b7d63c7f52a92c5df83f19fad9dc1265
Alex Gerstein <gerstein.alex@gmail.com>
2015-01-19 13:00:35 -0500

    Rename to config folder. Add local instance for vars.

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__, instance_relative_config=True)
 
 # Load the default configuration
-app.config.from_object('instance.default')
+app.config.from_object('config.default')
+
+# Load the configuration from the instance folder
+app.config.from_pyfile('config.py')
 
 # Load the file specified by the APP_CONFIG_FILE environment variable
 # Variables defined here will override those in the default configuration
