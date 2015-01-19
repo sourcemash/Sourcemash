1af88ac499cf76f9629b1de5af0f155814834985
Alex Gerstein <gerstein.alex@gmail.com>
2015-01-18 20:13:07 -0500

    Fix missing userfactory.

--- tests/api_tests/feed_tests.py
+++ tests/api_tests/feed_tests.py
@@ -21,20 +21,8 @@ class TestFeedAPI():
 		db.create_all()
 		feed = FeedFactory()
 
-<<<<<<< HEAD
 		self.feed_uri = '/api/feeds/%d' % feed.id
 		self.title = feed.title
-=======
-		app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-            os.path.join(basedir, 'test.db')
-        db.create_all()
-        FeedFactory()
-
-	def test_get_feed_present(self):
-		feed_uri = '/api/feeds/%d' % Feed.query.first().id
->>>>>>> Switch Feed API to use DB.
 
 	def test_get_feed_present(self):
 		r = self.app.get(self.feed_uri)
@@ -56,11 +44,7 @@ class TestFeedAPI():
 		eq_(data['result'], True)
 
 		# Dummy feed should no longer be reachable
-<<<<<<< HEAD
 		get = self.app.get(self.feed_uri)
-=======
-		get = self.app.get('/api/feeds/1')
->>>>>>> Switch Feed API to use DB.
 		eq_(get.status_code, 404)
 
 	def tearDown(self):
@@ -77,13 +61,6 @@ class TestFeedListAPI():
 			os.path.join(basedir, 'test.db')
 		db.create_all()
 		FeedFactory()
-
-		app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-            os.path.join(basedir, 'test.db')
-        db.create_all()
-        FeedFactory()
 
 	def test_get_feeds(self):
 		r = self.app.get('/api/feeds')
