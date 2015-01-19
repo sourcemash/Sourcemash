a3ae884a7175deeed302813f1cc1c4a9ebc17f88
Alex Gerstein <gerstein.alex@gmail.com>
2015-01-19 14:05:36 -0500

    Allow for no environment variable.

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,8 @@ app.config.from_pyfile('config.py', silent=True)
 
 # Load the file specified by the APP_CONFIG_FILE environment variable
 # Variables defined here will override those in the default configuration
-app.config.from_object('config.%s' % os.environ.get('APP_CONFIG_FILE'))
+if 'APP_CONFIG_FILE' in os.environ:
+	app.config.from_object('config.%s' % os.environ.get('APP_CONFIG_FILE'))
 
 db = SQLAlchemy(app)
 
