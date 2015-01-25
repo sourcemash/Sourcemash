82d931f7e409abf5bfeab7fe2027c027207afd71
Alex Gerstein <gerstein.alex@gmail.com>
2015-01-25 03:35:38 -0500

    Update Travis config for csrf.

--- config/travis.py
+++ config/travis.py
@@ -2,8 +2,9 @@ import logging
 import os
 
 TESTING = True
+LOGIN_DISABLED = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-CSRF_ENABLED = False
+WTF_CSRF_ENABLED = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 WTF_CSRF_SECRET_KEY = os.environ['WTF_CSRF_SECRET_KEY']
