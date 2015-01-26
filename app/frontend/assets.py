e7459e154be79cdb8b9a3bdd5762bf1b6b40457e
Alex Gerstein <gerstein.alex@gmail.com>
2015-01-25 19:01:07 -0500

    Add materialize and flask assets.

---
+++ app/frontend/assets.py
@@ -0,0 +1,16 @@
+from flask_assets import Environment, Bundle
+
+css_vendor = Bundle("scss/vendor/materialize/materialize.scss", 
+                    filters="pyscss", output="css/vendor.css")
+
+js_vendor = Bundle("js/vendor/materialize/*.js",
+                    filters="jsmin", output="js/vendor.min.js")
+
+def init_app(app):
+    webassets = Environment(app)
+    webassets.url = app.static_url_path
+    webassets.register('css_vendor', css_vendor)
+    webassets.register('js_vendor', js_vendor)
+    webassets.manifest = 'cache' if not app.debug else False
+    webassets.cache = not app.debug
+    webassets.debug = app.debug
--- app/frontend/dashboards.py
+++ app/frontend/dashboards.py
@@ -1,7 +1,7 @@
 from app import app
 
 from flask import abort, render_template, redirect
-from flask.ext.security import current_user
+from flask.ext.security import current_user, login_required
 
 from app.models import User
 
@@ -11,6 +11,7 @@ def index():
     return "Hello, World!"
 
 @app.route('/profile')
+@login_required
 def profile():
     return render_template('profile.html',
                             email=current_user.email)
