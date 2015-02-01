1c1fbcf3d6d2ddb4986ee4f8534747e40ab9c017
Alex Gerstein <gerstein.alex@gmail.com>
2015-01-31 22:01:14 -0500

    Make boxed.

--- test.sh
+++ test.sh
@@ -1,15 +1,6 @@
 export APP_CONFIG_FILE=testing
 
-ps aux | grep 'rss' | awk '{print $2}' | xargs kill
-
-echo "Start Flask app on port 5000..."
-python run.py &> flask.log &
-FLASK_PID=$!
-
 echo "Run tests...\n"
 py.test --boxed tests/
-
-echo "Kill Flask app..."
-kill $FLASK_PID
 
 rm test.db
