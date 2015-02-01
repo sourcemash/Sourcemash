f944280d793c6e777a9ad1c1b0da99b59064443f
Alex Gerstein <gerstein.alex@gmail.com>
2015-01-31 21:27:49 -0500

    Add SQL session stuff back.

--- app/database.py
+++ app/database.py
@@ -1,7 +1,39 @@
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy, SignallingSession, SessionBase
 from flask.ext.security import SQLAlchemyUserDatastore, Security
 
-db = SQLAlchemy()
+class _SignallingSession(SignallingSession):
+    """A subclass of `SignallingSession` that allows for `binds` to be specified
+    in the `options` keyword arguments.
+
+    """
+    def __init__(self, db, autocommit=False, autoflush=True, **options):
+        self.app = db.get_app()
+        self._model_changes = {}
+        self.emit_modification_signals = \
+            self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS']
+ 
+        bind = options.pop('bind', None)
+        if bind is None:
+            bind = db.engine
+ 
+        binds = options.pop('binds', None)
+        if binds is None:
+            binds = db.get_binds(self.app)
+ 
+        SessionBase.__init__(self,
+                             autocommit=autocommit,
+                             autoflush=autoflush,
+                             bind=bind,
+                             binds=binds,
+                             **options)
+ 
+ 
+class _SQLAlchemy(SQLAlchemy):
+    """A subclass of `SQLAlchemy` that uses `_SignallingSession`."""
+    def create_session(self, options):
+        return _SignallingSession(self, **options)
+ 
+db = _SQLAlchemy()
 from models import User, Role
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security()
