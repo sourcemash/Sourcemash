1f25a8c4fa8ca18885af4a96477245ad4786e583
Alex Gerstein <gerstein.alex@gmail.com>
2015-01-31 21:15:18 -0500

    Add role to new users.

--- tests/fixtures/user_fixtures.py
+++ tests/fixtures/user_fixtures.py
@@ -3,5 +3,6 @@ from tests.factories import user_factories, role_factories, feed_factories
 
 @pytest.yield_fixture(scope='function')
 def user(request):
+    role = role_factories.RoleFactory()
     feed = feed_factories.FeedFactory()
-    yield user_factories.UserFactory(subscribed=[feed])
+    yield user_factories.UserFactory(roles=[role], subscribed=[feed])
