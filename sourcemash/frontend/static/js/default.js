2ed4b80000209af8222e33d16f26927da3191363
Scott W. Gladstone <sgladstone11@gmail.com>
2015-02-08 15:59:11 -0500

    add feed event works with backbone

--- sourcemash/frontend/static/js/default.js
+++ sourcemash/frontend/static/js/default.js
@@ -2,40 +2,6 @@ $( document ).ready(function(){
 	$(".button-collapse").sideNav();
 });
 
-$('#add_feed_form').submit(function () {
-	$("#feed_url").siblings(".errors").text("")
-	$("#feed_url").blur()
-	append_feed()
-	$("#feed_url").val("");
-	return false;
-})
-
-append_feed = function() {
-	$.ajax({
-		type: "POST",
-		url: "/api/subscriptions",
-		data: {
-			feed_url : $("#feed_url").val()
-		},
-		dataType: "json",
-		error: function(data, textStatus, errorThrown) {
-			errors = data.responseJSON.errors
-			for (field in errors) {
-				input_field = $("#"+ field)
-				input_field.removeClass("valid");
-				input_field.addClass("invalid");
-				field_errors = input_field.siblings(".errors")
-				field_errors.text(errors[field])
-			}
-		},
-		success: function(data) {
-			var subscription = data.subscription;
-			subscription_view = "<p><a href='" + subscription.url + "'>" + subscription.title + "</a></p>";
-			$(".feeds").append(subscription_view);
-		}
-	})
-}
-
 'use strict';
 
 var feeds = {}; // create namespace for our app
@@ -45,16 +11,15 @@ var feeds = {}; // create namespace for our app
 //--------------
 feeds.Feed = Backbone.Model.extend({
   defaults: {
-    title: '',
     url: '',
-    last_updated: Date()
+    title: ''
   }
 });
 
 //--------------
 // Collections
 //--------------
-feeds.FeedList = Backbone.Collection.extend({
+feeds.FeedListCollection = Backbone.Collection.extend({
   model: feeds.Feed,
   url: '/api/subscriptions',
   parse: function(response) {
@@ -66,7 +31,7 @@ feeds.FeedList = Backbone.Collection.extend({
 });
 
 // instance of the Collection
-feeds.feedList = new feeds.FeedList();
+feeds.feedList = new feeds.FeedListCollection();
 
 //--------------
 // Views
@@ -84,38 +49,51 @@ feeds.FeedView = Backbone.View.extend({
 
 // renders the full list of todo items calling TodoView for each one.
 feeds.FeedsView = Backbone.View.extend({
-  el: '#feed_list',
+  el: '.feeds',
   initialize: function () {
-    this.input = this.$('#feed_url');
-    feeds.feedList.on('add', this.addAll, this);
-    feeds.feedList.on('reset', this.addAll, this);
-    feeds.feedList.fetch(); // Loads list from local storage
+    this.input = $('#feed_url');
+    feeds.feedList.on('sync', this.addAll, this);
+    feeds.feedList.fetch(); 
   },
   events: {
-    'keypress #feed_url': 'createFeedOnEnter'
+    'keypress #feed_url': 'keyPressEventHandler',
+    'click button[type="submit"]': 'createFeedOnEnter'
   },
   createFeedOnEnter: function(e){
-    if ( e.which !== 13 || !this.input.val().trim() ) { // ENTER_KEY = 13
+    e.stopPropagation();
+    feeds.feedList.create(this.newAttributes(), {wait: true});
+    this.input.val(''); // clean input box
+    feeds.feedList.fetch(); // forced refresh
+    return false;
+  },
+  keyPressEventHandler: function(e){
+  	if ( e.which !== 13 || !this.input.val().trim() ) { // ENTER_KEY = 13
       return;
     }
-    feeds.FeedList.create(this.newAttributes());
+    feeds.feedList.create(this.newAttributes(), {wait: true});
     this.input.val(''); // clean input box
+    feeds.feedList.fetch(); // forced refresh
+    return false;
   },
   addOne: function(feed){
     var view = new feeds.FeedView({model: feed});
     $('#feed-list').append(view.render().el);
   },
   addAll: function(){
-    this.$('#feed-list').html(''); // clean the todo list
+    $('#feed-list').html(''); // clean the todo list
     feeds.feedList.each(this.addOne, this);
   },
   newAttributes: function(){
     return {
-      url: this.input.val(),
-      title: "hey there"
+      feed_url: this.input.val()
     }
   }
 });
+
+Backbone.View.prototype.close = function() {
+	this.remove();
+	this.unbind();
+};
 
 //--------------
 // Initializers
