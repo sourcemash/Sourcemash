aa71ad09adf63eab3a22762b63ae55e27a1eb895
Alex Gerstein <gerstein.alex@gmail.com>
2015-02-18 23:58:15 -0500

    Add renamed files for tests.

---
+++ tests/test_api/test_feeds.py
@@ -0,0 +1,118 @@
+import pytest
+import json
+
+from . import TestBase
+
+def check_valid_header_type(headers):
+    assert headers['Content-Type'] == 'application/json'
+
+class TestFeedAPI(TestBase):
+
+    def test_get_feed_present(self, test_client, feed):
+        r = test_client.get('/api/feeds/%d' % feed.id)
+        check_valid_header_type(r.headers)
+        assert r.status_code == 200
+
+        data = json.loads(r.data)
+        assert data['feed']['title'] == feed.title
+
+    def test_get_feed_missing(self, test_client):
+        r = test_client.get('/api/feeds/0')
+        check_valid_header_type(r.headers)
+        assert r.status_code == 404
+
+    def test_delete_subscription_present(self, test_client, userWithFeed):
+        feed = userWithFeed.subscribed.first()
+
+        self.login(test_client, userWithFeed.email, userWithFeed.password)
+
+        unsubscribe = test_client.delete('api/feeds/%d' % feed.id)
+        check_valid_header_type(unsubscribe.headers)
+        data = json.loads(unsubscribe.data)
+
+        assert data['result'] == True
+        assert userWithFeed.subscribed.count() == 0
+
+    def test_delete_subscription_invalid_feed_id(self, test_client, userWithFeed):
+        feed = userWithFeed.subscribed.first()
+
+        self.login(test_client, userWithFeed.email, userWithFeed.password)
+
+        unsubscribe = test_client.delete('api/feeds/%d' % (int(feed.id)+1))
+        check_valid_header_type(unsubscribe.headers)
+        assert unsubscribe.status_code == 404   
+
+
+class TestFeedListAllAPI:
+
+    def test_get_feeds(self, test_client, feed):
+        r = test_client.get('/api/feeds/all')
+        check_valid_header_type(r.headers)
+        assert r.status_code == 200
+
+        data = json.loads(r.data)
+        assert len(data['feeds']) == 1
+    
+
+class TestFeedListAPI(TestBase):
+
+    def test_get_users_subscriptions(self, test_client, userWithFeed):
+        self.login(test_client, userWithFeed.email, userWithFeed.password)
+
+        user_feed = userWithFeed.subscribed.first()
+
+        r = test_client.get('/api/feeds')
+        check_valid_header_type(r.headers)
+        assert r.status_code == 200
+
+        data = json.loads(r.data)
+        assert len(data['feeds']) == 1
+
+        assert data['feeds'][0]['title'] == user_feed.title
+
+    def test_post_subscription_validURL_new_feed(self, test_client, user):
+        self.login(test_client, user.email, user.password)
+
+        subscription_data = dict(url='http://online.wsj.com/xml/rss/3_7085.xml')
+        r = test_client.post('/api/feeds', data=subscription_data)
+
+        check_valid_header_type(r.headers)
+        assert r.status_code == 201
+        assert user.subscribed.first().url == 'http://online.wsj.com/xml/rss/3_7085.xml'
+
+    def test_post_subscription_validURL_old_feed(self, test_client, user, real_feed):
+        self.login(test_client, user.email, user.password)
+
+        subscription_data = dict(url=real_feed.url)
+        r = test_client.post('/api/feeds', data=subscription_data)
+
+        check_valid_header_type(r.headers)
+        assert r.status_code == 201
+
+    def test_post_subscription_invalid_feed_url(self, test_client, user):
+        self.login(test_client, user.email, user.password)
+
+        subscription_data = dict(url='http://nonexistentURL')
+        r = test_client.post('/api/feeds', data=subscription_data)
+
+        check_valid_header_type(r.headers)
+        assert r.status_code == 422
+
+        data = json.loads(r.data)
+        assert len(data['errors']['url']) == 1
+        assert 'not a valid feed' in data['errors']['url'][0]
+
+    def test_post_subscription_already_subscribed_feed(self, test_client, userWithRealFeed):
+        self.login(test_client, userWithRealFeed.email, userWithRealFeed.password)
+
+        feed = userWithRealFeed.subscribed.first()
+
+        subscription_data = dict(url=feed.url)
+        r = test_client.post('/api/feeds', data=subscription_data)
+
+        check_valid_header_type(r.headers)
+        assert r.status_code == 409
+
+        data = json.loads(r.data)
+        assert len(data['errors']['url']) == 1
+        assert 'Already subscribed' in data['errors']['url'][0]
---
+++ tests/test_api/test_items.py
@@ -0,0 +1,93 @@
+import pytest
+import json
+
+from . import TestBase
+from tests.factories import item_factories
+
+
+def check_valid_header_type(headers):
+    assert headers['Content-Type'] == 'application/json'
+
+class TestItemAPI:
+
+    def test_get_item_present(self, test_client, item):
+        r = test_client.get('/api/items/%d' % item.id)
+        check_valid_header_type(r.headers)
+        assert r.status_code == 200
+
+        data = json.loads(r.data)
+        assert data['item']['title'] == item.title
+
+    def test_get_item_missing(self, test_client):
+        r = test_client.get('/api/items/%d' % 10)
+        check_valid_header_type(r.headers)
+        assert r.status_code == 404
+
+class TestFeedItemListAPI:
+
+    def test_get_items(self, test_client, item):
+        r = test_client.get('/api/feeds/%d/items' % item.feed.id)
+
+        check_valid_header_type(r.headers)
+        assert r.status_code == 200
+
+        data = json.loads(r.data)
+        assert len(data['items']) == 1
+
+
+class TestCategoryItemListAllAPI:
+
+    def test_get_category_items_present(self, test_client, itemWithCategory):
+        r = test_client.get('/api/categories/' + itemWithCategory.category_1 + '/items/all')
+        check_valid_header_type(r.headers)
+        assert r.status_code == 200
+
+        data = json.loads(r.data)
+        assert len(data['items']) == 1
+
+    def test_get_category_items_missing_category(self, test_client):
+        r = test_client.get('/api/categories/nonexistent_category/items/all')
+        check_valid_header_type(r.headers)
+        assert r.status_code == 200
+
+        data = json.loads(r.data)
+        assert len(data['items']) == 0
+
+    def test_multiple_items_in_one_category(self, test_client, itemsWithCategory):
+        r = test_client.get('/api/categories/' + itemsWithCategory[0].category_1 + '/items/all')
+
+        check_valid_header_type(r.headers)
+        assert r.status_code == 200
+
+        data = json.loads(r.data)
+        assert len(data['items']) == 5
+
+
+class TestCategoryItemListAPI(TestBase):
+
+    def test_get_users_items_present(self, test_client, userWithPopulatedFeed):
+        self.login(test_client, userWithPopulatedFeed.email, userWithPopulatedFeed.password)
+
+        feed = userWithPopulatedFeed.subscribed.first()
+
+        r = test_client.get('/api/categories/' + feed.items[0].category_1 + '/items')
+
+        check_valid_header_type(r.headers)
+        assert r.status_code == 200
+
+        data = json.loads(r.data)
+        assert len(data['items']) == 5
+
+
+    def test_get_users_matching_categories_missing(self, test_client, userWithPopulatedFeed):
+        self.login(test_client, userWithPopulatedFeed.email, userWithPopulatedFeed.password)
+
+        outside_item = item_factories.ItemWithCategoryFactory(category_1="vegetables")
+
+        r = test_client.get('/api/categories/' + outside_item.category_1 + '/items')
+
+        check_valid_header_type(r.headers)
+        assert r.status_code == 200
+
+        data = json.loads(r.data)
+        assert len(data['items']) == 0
