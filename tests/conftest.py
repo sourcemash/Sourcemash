5f7a06fb22d8e1b20ebbc5affdebbae41a69fb55
Alex Gerstein <alexgerstein@users.noreply.github.com>
2015-03-24 15:28:42 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 from fixtures.app_fixtures import *
 from fixtures.selenium_fixtures import *
 from fixtures.categorizer_fixtures import *
-
 from fixtures.user_fixtures import *
 from fixtures.item_fixtures import *
 from fixtures.user_item_fixtures import *
