6e0f6be2925ba5bc5c1e09ff54b6e9432d09a6f8
Scott W. Gladstone <sgladstone11@gmail.com>
2015-04-22 23:28:20 -0400

    register view

--- sourcemash/frontend/static/js/views/register_modal_view.js
+++ sourcemash/frontend/static/js/views/register_modal_view.js
@@ -9,10 +9,7 @@ Sourcemash.Views.RegisterModalView = Backbone.View.extend({
         e.preventDefault();
         var formData = JSON.stringify($("#register").serializeObject());
         $('#register-password').val('');
-<<<<<<< HEAD
         $('#register-password-confirm').val('');
-=======
->>>>>>> register form ajax call. doesn't work locally but i have a feeling it will work on staging/production
         var posting = $.ajax({
                           type: "POST",
                           url: "/register",
@@ -25,11 +22,7 @@ Sourcemash.Views.RegisterModalView = Backbone.View.extend({
     showErrors: function(data) {
         var user = data.response.user;
         if (user) {
-<<<<<<< HEAD
           mixpanel.track("Register", {"User": user});
-=======
-          mixpanel.track("Register");
->>>>>>> register form ajax call. doesn't work locally but i have a feeling it will work on staging/production
           $("#register-modal").closeModal();
           toast("Check your email for confirmation!", 5000);
         };
@@ -37,11 +30,7 @@ Sourcemash.Views.RegisterModalView = Backbone.View.extend({
         if (errors) {
           errorMsg = errors.email || errors.password || errors.password_confirm || {};
           $("#register-errors").html(errorMsg);
-<<<<<<< HEAD
         };
-=======
-        }
->>>>>>> register form ajax call. doesn't work locally but i have a feeling it will work on staging/production
     },
 
     render: function() {
