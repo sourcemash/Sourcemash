d88cfe260c759dd2e72160a60ad3290143b689ea
Scott W. Gladstone <sgladstone11@gmail.com>
2015-04-26 20:10:49 -0400

    favicon only

--- sourcemash/frontend/static/js/views/splash_view.js
+++ sourcemash/frontend/static/js/views/splash_view.js
@@ -1,8 +1,3 @@
 Sourcemash.Views.SplashView = Backbone.View.extend({
     template: JST['splash'],
-
-    render: function() {
-      this.$el.html(this.template());
-      return this;
-    }
 });
--- sourcemash/frontend/templates/splash.tpl
+++
@@ -1,6 +0,0 @@
-<div class="row">
-  <div class="col s12 valign-wrapper">
-    <img src="/static/solologo.svg">
-    <img src="/static/logotext.svg">
-  </div>
-</div>
