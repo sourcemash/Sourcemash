29b58f338baa5dac0f312823c920980140fc7fa3
Scott Gladstone <swglad@users.noreply.github.com>
2015-05-04 14:49:09 -0400

    Update item_card_view.js

--- sourcemash/frontend/static/js/views/item_card_view.js
+++ sourcemash/frontend/static/js/views/item_card_view.js
@@ -99,7 +99,7 @@ Sourcemash.Views.ItemCardView = Backbone.View.extend({
         if (this.user.get('email')) {
             shownCategories = this.model.categories.filter(function(category) {
               return category.get("item_count") > 1;
-            }).slice(0, 5);
+            }).slice(0, Sourcemash.Views.ItemCardView.MAX_CATEGORIES_SHOWN);
         } else {
             shownCategories = this.model.categories.first(Sourcemash.Views.ItemCardView.MAX_CATEGORIES_SHOWN);
         }
