e5e832c82ae1d4aa74bbd3cc01a0a117eb4ce7eb
Alex Gerstein <gerstein.alex@gmail.com>
2015-05-10 21:04:25 -0400

    Fix migration ordering.

--- migrations/versions/2780dc80a7c0_.py
+++ migrations/versions/2780dc80a7c0_.py
@@ -1,14 +1,14 @@
 """empty message
 
 Revision ID: 2780dc80a7c0
-Revises: 2ad0f09d35de
+Revises: 293250007b39
 Create Date: 2015-05-07 14:15:50.002172
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '2780dc80a7c0'
-down_revision = '2ad0f09d35de'
+down_revision = '293250007b39'
 
 from alembic import op
 import sqlalchemy as sa
