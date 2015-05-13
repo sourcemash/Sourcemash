1fa558d469fbde56cc090fbea079b88b2081ea92
Alex Gerstein <gerstein.alex@gmail.com>
2015-05-12 22:57:27 -0400

    Add tests.

--- sourcemash/admin.py
+++ sourcemash/admin.py
@@ -1,3 +1,4 @@
+from flask import redirect, url_for
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqla import ModelView
 from flask.ext.login import current_user
@@ -14,6 +15,10 @@ class FeedView(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated() and \
                current_user.email == "admin@sourcemash.com"
+
+    def _handle_view(self, name, **kwargs):
+            if not self.is_accessible():
+                return redirect(url_for('security.login'))
 
     column_display_pk = True
     form_columns = ['title', 'topic', 'public', 'image_url']
